f9c68a59d4c2f81eacd7ba629c937ae4a8913940
bebegreen <binyamin.green@gmail.com>
2018-02-16 19:24:26 +0200

    ..

--- server/models/config/db.conf.js
+++ server/models/config/db.conf.js
@@ -1,14 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = require('./dbUrl.js').url;
-const objectID = require('mongodb').ObjectID;
 const connection = require('./dbUrl.js').connection;
-const locations = require('./mock-locations.js').locations; 
-const users = require('./mock-locations.js').users; 
 
 
 async function createDB() {
   const db = await connection.then(db => db);
-  console.log('restarted database');
+
+  console.log(db, 'restarted database');
 }
 
 module.exports = createDB;
