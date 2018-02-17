59e17279d96b293998fe865fcec57a9207bfa89b
bebegreen <binyamin.green@gmail.com>
2018-02-17 18:39:29 +0200

    ..

--- server/controllers/validateSignup.js
+++ server/controllers/validateSignup.js
@@ -1,5 +1,4 @@
 const Joi = require('joi');
-const saltRounds = 10;
 
 const schema = Joi.object().keys({
   firstName: Joi.string(),
--- server/models/signups.js
+++ server/models/signups.js
@@ -2,13 +2,13 @@
 const connection = require('./config/dbUrl.js').connection;
 
 exports.signup = (user) => {
-  return connection.then(db => { 
-    return db.collection('signups').insertOne(user); 
-  })
+  return connection.then(db => {
+    return db.collection('signups').insertOne(user);
+  }); 
 }
 
 exports.getAllSignups = () => { 
-  return connection.then(db => { 
-    return db.collection('signups').find({}).toArray(); 
-  })
+  return connection.then(db => {
+    return db.collection('signups').find({}).toArray();
+  }); 
 }
